80a7fde2cd071a74a13d197e2a58d7922ac23616
王 憲寿 <s.wau@lsm.co.jp>
2016-02-02 20:00:59 +0900

    201602022000 wang.xianshou modify

---
+++ WindowsApplication2/WindowsApplication2/OUTPUT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_SHARP_STUDY
+{
+    class PARA
+    {
+        //实体类这部分可以分成两种类型
+        //1.页面传递给业务的处理参数
+        //2.业务处理完毕返回给页面的显示数据
+        private int _id;
+        public int ID
+        {
+            get { return _id;}
+            set { _id = value; }
+        }
+    }
+}
--- WindowsApplication2/WindowsApplication2/PARA.cs
+++ WindowsApplication2/WindowsApplication2/PARA.cs
@@ -4,16 +4,37 @@ using System.Text;
 
 namespace C_SHARP_STUDY
 {
-    class PARA
+    public class OUTPUT
     {
         //实体类这部分可以分成两种类型
         //1.页面传递给业务的处理参数
         //2.业务处理完毕返回给页面的显示数据
-        private int _id;
-        public int ID
+
+        public int Status
         {
-            get { return _id;}
-            set { _id = value; }
+            get;
+            set;
         }
+        public string Message
+        {
+            get;
+            set;
+        }
+        public string Result1
+        {
+            get;
+            set;
+        }
+        public string Result2
+        {
+            get;
+            set;
+        }
+        public string Result3
+        {
+            get;
+            set;
+        }
+        
     }
 }
